1da3c51a5079d204e1e4fbf4d51c99688cc39a33
Vasile-Alin Apostu <vasilealin54@gmail.com>
2025-05-12 23:47:21 +0300

    add a short story

--- src/Components/HomePage/Content.tsx
+++ src/Components/HomePage/Content.tsx
@@ -2,8 +2,10 @@ import style from "../../Style/Pages/HomePage.module.css";
 import CustomSuiteSvg from "../../Images/customSuits.png";
 import PresicionSvg from "../../Images/precision.png";
 import PremiumFabricsSvg from "../../Images/premiumFabrics.png";
+import { useNavigate } from "react-router-dom";
 
 const Content = (): React.JSX.Element => {
+  const navigate = useNavigate();
   return (
     <div className={style["content-container"]}>
       <div className={style["what-we-do"]}>
@@ -22,12 +24,23 @@ const Content = (): React.JSX.Element => {
           <p className={style["hover-text"]}>Premium fabrics</p>
         </div>
       </div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
+      <div className={style["our-story"]}>
+        <h4 className={style["story"]}>
+          In a world of fleeting fashion, we chose permanence. Founded in 2025,
+          our atelier was born from a love for quiet craftsmanship and refined
+          menswear.
+          <br /> Each piece is made with intention—for men who value precision,
+          character, and timeless design.
+        </h4>
+        <button
+          onClick={() => {
+            navigate("/about");
+          }}
+          className={style["button"]}
+        >
+          Read Our Story →
+        </button>
+      </div>
     </div>
   );
 };
